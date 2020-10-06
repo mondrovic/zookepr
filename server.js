5813b45219ddd09515612205ece7981fa2518b05
Matthew O <mo@pop-os.localdomain>
2020-10-05 20:09:23 -0700

    Add heroku

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const { animals } = require("./data/animals");
+
+const app = express();
+
+app.listen(3001, () => {
+  console.log("API server now on port 3001");
+});
+
+function filterByQuery(query, animalArr) {
+  let personalityTraits = [];
+  let filteredResults = animalArr;
+
+  if (query.personalityTraits) {
+    if (typeof query.personalityTraits === "string") {
+      personalityTraits = [query.personalityTraits];
+    } else {
+      personalityTraits = query.personalityTraits;
+    }
+  }
+
+  personalityTraits.forEach((trait) => {
+    filteredResults = filteredResults.filter(
+      (animal) => animal.personalityTraits.indexOf(trait) !== -1
+    );
+  });
+
+  if (query.diet) {
+    filteredResults = filteredResults.filter(
+      (animal) => animal.diet === query.diet
+    );
+  }
+
+  if (query.species) {
+    filteredResults = filteredResults.filter(
+      (animal) => animal.species === query.species
+    );
+  }
+
+  if (query.name) {
+    filteredResults = filteredResults.filter(
+      (animal) => animal.name === query.name
+    );
+  }
+
+  return filteredResults;
+}
+
+// takes in req (animal)
+app.get("/api/animals", (req, res) => {
+  let results = animals;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
